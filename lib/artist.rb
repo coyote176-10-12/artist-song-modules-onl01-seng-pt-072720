0a710b135d20aec64bbe672a5856bda65889e0b9
Javier Aguilar <coyote176@msn.com>
2020-08-30 05:13:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods 
   extend Findable
+  include Memorable::InstanceMethods
   include Paramable
   attr_accessor :name
   attr_reader :songs
