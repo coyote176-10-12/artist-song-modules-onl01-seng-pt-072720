2658ac9840ecc7c0d20caed706abead181609e15
Javier Aguilar <coyote176@msn.com>
2020-08-30 01:42:39 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -8,10 +8,8 @@ module Findable
     #@songs.detect{|a| a.name == name}
  # end
   
-    def find_by_name(name)
-      self.all.detect {|a| a.name == name}
-    end
-    
-    
+ def find_by_name(name)
+    all.detect{|a| a.name == name}
+  end
     
 end
